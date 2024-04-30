a7f08be96e26fcd6eda7f871df6539baf619180d
javiermelom <javiermelom@hotmail.com>
2024-04-30 15:50:43 -0500

    Realizando ejercicio 01 crear un cuadrado con prints

---
+++ retos_sesion_04/ejercicio_01.py
@@ -0,0 +1,6 @@
+# Crea un archivo "ejercicio_01.py", añade un comentario con la descripción del ejercicio "1" y crea un cuadrado con prints
+print ("********")
+print ("*      *")
+print ("*      *")
+print ("*      *")
+print ("********")
