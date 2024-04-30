2c13d2b981ccadb65ca22b7a765832581a8da436
javiermelom <javiermelom@hotmail.com>
2024-04-30 16:08:58 -0500

    Reglas juego 21 o blackjack

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,7 @@
+# Juego 21 o blackjack
+print ("1. La casa te entrega 2 cargas tapadas")
+print ("2. La casa se asigna 2 cartas una tapada y otra destapada")
+print ("3. Debes sumar el numero que tienen las dos cargas que te asigna la casa y tratar de sumar 21 sin pasarte de esta cifra")
+print ("4. Para sumar 21 con las cartas que tienes y las pedidas a la casa adicionales")
+print ("5. Al momento de parar la solicitud de cartas a la casa debes mostrar todas tus cartas")
+print ("6. La casa destapa la carta que desde el inicio estaba tapada y empieza a asignarse mas cartas si el valor no sobrepasa")
