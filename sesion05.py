5fda8f2f8a4c4e400eaaaa61c263610812785836
javiermelom <javiermelom@hotmail.com>
2024-04-30 20:30:35 -0500

    tipos de datos y datos numericos

---
+++ sesion05.py
@@ -0,0 +1,66 @@
+print ( type (1) )
+# Valor 10 en base decimal
+print ("Base decimal")
+print (10)
+# Valor 10 en binario
+print ("Base binaria")
+print (0b1010)
+# Valor 10 en octal
+print ("Base octal")
+print (0o12)
+# Valor 10 en hexadecimal
+print ("Base hexadecimal")
+print (0xa)
+
+# Operadores aritméticos
+a = 10
+b = 3
+# Suma
+print ("Suma")
+print (a + b)
+# Resta
+print ("Resta")
+print (a - b)
+# Multiplicación
+print ("Multiplicación")
+print (a * b)
+# División
+print ("División")
+print (a / b)
+# Potencia
+print ("Potencia")
+print (a ** b)
+# Módulo o residuo
+print ("Módulo o residuo")
+print (a % b)
+# División entera
+print ("División entera")
+print (a // b)
+
+# Un contador tiene 300 minutos y se le suman 3600 segundos, ¿Cuántas horas en total son?
+tiempo_actual = 300
+tiempo_adicional = 3600
+tiempo_total = (tiempo_actual + (3600 / 60)) / 60
+print ("Horas:",tiempo_total)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
