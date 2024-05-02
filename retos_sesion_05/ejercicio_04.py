26ffade25696811c15a9be7c003a581f8c1b3be3
javiermelom <javiermelom@hotmail.com>
2024-05-02 14:00:53 -0500

    Ejecucion ejercicio_05.ipynb

--- retos_sesion_05/ejercicio_04.py
+++ retos_sesion_05/ejercicio_04.py
@@ -1,1 +1,5 @@
 #Calcular el volumen de un cubo donde uno de sus lados mide 10 metros, utilizando la fórmula general del volumen de un cubo
+
+side = 10
+volumen = side ** 3
+print ("The volume of cube with side 10 meters is:",volumen,"meters")
