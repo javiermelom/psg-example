e88960446de2a89cf258024efca4e72f894047a0
javiermelom <javiermelom@hotmail.com>
2024-05-06 18:07:29 -0500

    Solucion retos sesion 06 ejercicio04.py logica de programacion

--- retos_sesion_06/ejercicio_04.py
+++ retos_sesion_06/ejercicio_04.py
@@ -1,1 +1,6 @@
 # La suma de los números 7 y 3 es un número par?
+num1 = 7
+num2 = 3
+suma = num1 + num2
+result = (suma % 2 != True)
+print ("El resultado de la suma de",num1,"y",num2,"es par?\n",result)
