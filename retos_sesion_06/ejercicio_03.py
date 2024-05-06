ba77df701a5b47e9dd959649d108bc539dd3566c
javiermelom <javiermelom@hotmail.com>
2024-05-06 17:57:25 -0500

    Solucion retos sesion 06 ejercicio03.py logica de programacion

--- retos_sesion_06/ejercicio_03.py
+++ retos_sesion_06/ejercicio_03.py
@@ -1,3 +1,18 @@
 # Imprime una tabla de verdad para la siguiente afirmación: "Una puerta tiene dos interruptores que controlan dos luces,
 # la puerta sólo debe abrirse cuando las dos luces están apagadas o las dos están encendidas, caso contrario la puerta no se abre, 
 # ¿qué operador lógico se puede utilizar?"
+print ("TABLA DE VERDAD")
+switch1 = True
+switch2 = True
+print (not ((switch1 or switch2) and not (switch1 and switch2)))
+switch1 = True
+switch2 = False
+print (not ((switch1 or switch2) and not (switch1 and switch2)))
+switch1 = False
+switch2 = True
+print (not ((switch1 or switch2) and not (switch1 and switch2)))
+switch1 = False
+switch2 = False
+print (not ((switch1 or switch2) and not (switch1 and switch2)))
+
+print ("El operador logico que se utiliza es el XNOR")
