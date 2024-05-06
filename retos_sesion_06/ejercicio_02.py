aaa2029dc1c440808f146d26ba111c50c329aca7
javiermelom <javiermelom@hotmail.com>
2024-05-06 17:37:40 -0500

    Retos sesion 06 / ejercicio_02.py creacion de XNOR

--- retos_sesion_06/ejercicio_02.py
+++ retos_sesion_06/ejercicio_02.py
@@ -1,1 +1,27 @@
 # Construir el operador XNOR en Python
+
+print ("Operador OR")
+a = True
+b = False
+print (a or a)
+print (a or b)
+print (b or a)
+print (b or b)
+print()
+
+print ("Operador XOR")
+a = True
+b = False
+print ((a or b) and not (a and b))
+a = True
+b = True
+print ((a or b) and not (a and b))
+
+print()
+print ("Operador NXOR")
+a = True
+b = False
+print (not ((a or b) and not (a and b)))
+a = True
+b = True
+print (not ((a or b) and not (a and b)))
