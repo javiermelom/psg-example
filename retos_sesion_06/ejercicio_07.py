538cb0805f85cf9557eac4e9f90f95a33a71001f
javiermelom <javiermelom@hotmail.com>
2024-05-06 20:52:03 -0500

    Retos sesion 06 ejercicio_06.py

--- retos_sesion_06/ejercicio_07.py
+++ retos_sesion_06/ejercicio_07.py
@@ -1,1 +1,8 @@
 # ¿El cuadrado y la suma de 2 y 2 son iguales?
+num1 = 2
+num2 = 2
+cuadrado = num1 * num2
+suma = num1 + num2
+print ("\n",suma,"\n",cuadrado)
+print ("El cuadrado y la suma de",num1,"y",num2,"son iguales?")
+print (cuadrado == suma)
