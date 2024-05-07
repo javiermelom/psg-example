a99cd6f4d41b1c5bd394c62cf72b10421e2b1306
javiermelom <javiermelom@hotmail.com>
2024-05-06 20:45:45 -0500

    Retos sesion 06 ejercicio06.py

--- retos_sesion_06/ejercicio_06.py
+++ retos_sesion_06/ejercicio_06.py
@@ -1,1 +1,5 @@
 # ¿El cuadrado de 123 se encuentra en el siguiente rango? [N > 0 & N < 20000]
+num = 123
+cuadrado_de_num = num * num
+print (cuadrado_de_num)
+print ("El cuadrado de",num,"se encuentra entre el sig rango (0 - 20.000)?\n",cuadrado_de_num > 0 & cuadrado_de_num < 20000)
