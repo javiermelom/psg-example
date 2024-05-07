c35aaece98a0caaf151d8d2d70e0be7501899869
javiermelom <javiermelom@hotmail.com>
2024-05-06 20:33:49 -0500

    Retos sesion 06 ejercicio05.py

--- retos_sesion_06/ejercicio_05.py
+++ retos_sesion_06/ejercicio_05.py
@@ -1,5 +1,8 @@
 # Comparar los números 123 y 890, comprobar si tienen la misma paridad ambos pares o ambos impares
-# ¿El cuadrado de 123 se encuentra en el siguiente rango? [N > 0 & N < 20000]
-# ¿El cuadrado y la suma de 2 y 2 son iguales?
-
-# Convertir y ejecutar el archivo de la "sesion06.py" a un archivo en jupyter notebook
+num1 = 123
+num2 = 890
+num_1 = (num1 % 2 != 0)
+num_2 = (num2 % 2 != 0)
+print (num1)
+print (num2)
+print ("Es",(num_1 == num_2),"que los numeros",num1,"y",num2,"tengan la misma paridad")
