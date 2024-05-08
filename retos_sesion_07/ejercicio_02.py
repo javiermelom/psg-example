3154be6061e786f951188390270188e4c0242502
javiermelom <javiermelom@hotmail.com>
2024-05-07 20:00:42 -0500

    Ejercicio 02

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,5 @@
+# Escribe un programa que reciba dos enteros por teclado y sume su resultado
+num1 = int(input("Digite primer valor entero a sumar: "))
+num2 = int(input("Digite segundo valor entero a sumar: "))
+resultado = num1 + num2
+print ("La suma de",num1,"y",num2,"es:",resultado)
