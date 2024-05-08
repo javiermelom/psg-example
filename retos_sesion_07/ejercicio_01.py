643b237744cf90b5766ece863b636b03e0511df5
javiermelom <javiermelom@hotmail.com>
2024-05-07 19:56:38 -0500

    Ejercicio 01

---
+++ retos_sesion_07/ejercicio_01.py
@@ -0,0 +1,3 @@
+# Escribir una cadena con una frase de una canción o un poema
+string = "Welcome to the Hotel California Such a lovely place (such a lovely place) Such a lovely face"
+print (string)
