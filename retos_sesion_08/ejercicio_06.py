36c6e9329254d5e2b1294b217591a417b99e95f9
javiermelom <javiermelom@hotmail.com>
2024-05-13 20:41:11 -0500

    Ejercicio 06 tuplas

---
+++ retos_sesion_08/ejercicio_06.py
@@ -0,0 +1,10 @@
+# De las tuplas de coordenadas
+# (x1,y1)=(40,80) y (x2,y2)=(50,50)
+# Encuentra las coordenadas del punto medio, almacena en una tupla e imprime el resultado
+# (x,y)
+coordenada1 = (40,80)
+coordenada2 = (50,50)
+punto_medio_x = (coordenada1[0] + coordenada2[0])/2
+punto_medio_y = (coordenada1[1] + coordenada2[1])/2
+coord_punto_medio = (punto_medio_x, punto_medio_y)
+print ("Coordenada del punto medio es:",coord_punto_medio)
