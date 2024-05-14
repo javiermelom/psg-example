3a61fe67b572171b3b181534553eabf024a2c970
javiermelom <javiermelom@hotmail.com>
2024-05-13 20:29:53 -0500

    Ejercicio 05 tupla anidada

---
+++ retos_sesion_08/ejercicio_05.py
@@ -0,0 +1,12 @@
+# De la siguiente guía telefónica
+# Juan, 591123456, La Paz
+# Pedro, 591546324, La Paz
+# Maria, 591222421, El Alto
+# Sol, 591753159, Oruro
+# Genera una tupla anidada e imprime el resultado
+tupla_Juan = ("Juan", 591123456, "La Paz")
+tupla_Pedro = ("Pedro", 591546324, "La Paz")
+tupla_Maria = ("Maria", 591222421, "El Alto")
+tupla_Sol = ("Sol", 591753159, "Oruro")
+tupla_anidada = (tupla_Juan, tupla_Pedro, tupla_Maria, tupla_Sol)
+print ("Tupla anidada:",tupla_anidada)
