74fc966ee7d707b800da7273f9b8d731675a5b52
javiermelom <javiermelom@hotmail.com>
2024-05-13 19:51:34 -0500

    Ejercicio_01 tuplas

---
+++ retos_sesion_08/ejercicio_01.py
@@ -0,0 +1,6 @@
+# Ingresa por teclado dos coordenadas 'x','y' y una ubicación, almacena los 3 valores en una tupla e imprime el resultado
+cord_x = float(input("Digite el valor de coordenada x: "))
+cord_y = float(input("Digite valor de coordenada y: "))
+location = input("Digite la ubicacion: ")
+tupla = (cord_x, cord_y, location)
+print ("La tupla con los datos ingresados es:",tupla)
