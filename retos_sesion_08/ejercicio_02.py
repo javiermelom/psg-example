cf5f5b39b52adaef6785c4ad25e075564c87a45c
javiermelom <javiermelom@hotmail.com>
2024-05-13 20:01:16 -0500

    Ejercicio_02 tuplas

---
+++ retos_sesion_08/ejercicio_02.py
@@ -0,0 +1,12 @@
+# Crea una tupla con los siguientes elementos 1,2,3,4,5,6,7,8,9,10 y realiza:
+# Imprime el primer elemento
+# Imprime el último elemento
+# Imprime un slice del 4 al 7
+# Imprime un slice del 2 al 9 con pasos de 3
+# Imprime un slice del 10 al 1 con saltos de -2
+tupla = (1,2,3,4,5,6,7,8,9,10)
+print ("Primer elemento de la tupla:",tupla[0])
+print ("Ultimo elemento de la tupla:",tupla[-1])
+print ("Slice del 4 al 7:",tupla[4:7])
+print ("Slice del 2 al 9 con paso de 3:",tupla[2:9:3])
+print ("Slice del 10 al 1 con salos de -2:",tupla[10:1:-2])
