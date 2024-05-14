62ca877901632a0cd8edc0157b80348166903649
javiermelom <javiermelom@hotmail.com>
2024-05-13 20:15:12 -0500

    Ejercicio 03 tuplas concatenacion

---
+++ retos_sesion_08/ejercicio_03.py
@@ -0,0 +1,11 @@
+# Ingresa una cadena por teclado y almacena el valor en una tupla
+# Concatena la tupla ('¡', ) + tupla almacenada + la tupla ('!', )
+# Imprime el resultado concatenado
+# Repite la tupla final 3 veces e imprime el nuevo resultado
+string = input("Digite una cadena de caracteres:")
+tupla_string = (string)
+tupla_start = ("¡")
+tupla_end = ("!")
+concatena = tupla_start + tupla_string + tupla_end
+print (concatena)
+print (concatena * 3)
