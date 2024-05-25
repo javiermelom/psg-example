c045977464c1188eff9e15feeaa8f19f8b4f0b9d
javiermelom <javiermelom@hotmail.com>
2024-05-24 21:13:01 -0500

    Ejercicio 6 condicionales. Calculadora, revisar validacion de numeros y tambien operador segunda forma de verificar operador

---
+++ retos_sesion_12/ejercicio_06.py
@@ -0,0 +1,29 @@
+# Crea una calculadora por consola que realice las operaciones de suma, resta, multiplicación y división, 
+# ingresa dos números y la operación a realizar, verifica si la operación es válida y muestra el resultado
+# Número 1: 10
+# Número 2: 5
+# Operación: +
+# ------------
+# Resultado: 15
+num1 = float(input("Ingresa primer numero: "))
+operator = input("Ingresa que operacion quieres realizar: \n (+, -, *, /): ")
+num2 = float(input("Ingresa segundo numero: "))
+print (operator, type(operator))
+
+if (operator == "+"):
+    result = num1 + num2
+    print (f"El resultado de sumar {num1} + {num2} es: {result}")
+elif (operator == "-"):
+    result = num1 - num2
+    print (f"El resultado de restar {num1} - {num2} es: {result}")
+elif (operator == "*"):
+    result = num1 * num2
+    print (f"El resultado de multiplicar {num1} * {num2} es: {result}")
+elif (operator == "/"):
+    if num2:
+        result = num1 / num2
+        print (f"El resultado de dividir {num1} / {num2} es: {result}")
+    else:
+        print ("Num2 es cero y no se puede dividir por cero")
+else:
+    print ("Ingreso un operador Invalido")
