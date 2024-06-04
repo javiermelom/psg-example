09b87228dbfbd34e33f6e6fa88bc8ddc1226231e
javiermelom <javiermelom@hotmail.com>
2024-06-03 23:20:48 -0500

    Ejercicio 5 bucles matriz anidada, con ayuda de chatgpt

--- retos_sesion_13/ejercicio_05.py
+++ retos_sesion_13/ejercicio_05.py
@@ -1,1 +1,10 @@
 # Imprimir un tablero de ajedrez de 8x8 con los caracteres # y *
+n = 8
+for i in range(n):
+    fila = ""
+    for j in range(n):
+        if (i + j) % 2 == 0:
+            fila += "* "
+        else:
+            fila += "# "
+    print(fila)
