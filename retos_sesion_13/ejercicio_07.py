c031c04a34628c36de44514ba88116c29245aa02
javiermelom <javiermelom@hotmail.com>
2024-06-03 23:48:29 -0500

    Ejercicio 7 bucles y condiciones

--- retos_sesion_13/ejercicio_07.py
+++ retos_sesion_13/ejercicio_07.py
@@ -1,2 +1,12 @@
 # Crear una serie de números del 1 al 100, si el número es divisible por 3 imprimir Fizz, 
 # si el número es divisible por 5 imprimir Buzz, si el número es divisible por 3 y 5 imprimir FizzBuzz
+for i in range (101):
+    if i % 3 == 0:
+        if i % 5 == 0:
+            print (f"{i} FizzBuzz")
+        else:
+            print (f"{i} Fizz")
+    elif i % 5 == 0:
+        print (f"{i} Buzz")
+    else:
+        print (f"{i} No aplica")
