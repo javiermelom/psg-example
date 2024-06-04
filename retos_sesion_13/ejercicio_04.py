5a5e0a81ebf044554ef86a6ab1fa8e117909195a
javiermelom <javiermelom@hotmail.com>
2024-06-03 20:58:37 -0500

    Ejercicio 4 bucles y cliclos infinitos, se demora en responder el programa con este codigo

--- retos_sesion_13/ejercicio_04.py
+++ retos_sesion_13/ejercicio_04.py
@@ -1,2 +1,10 @@
 # Crea un ciclo infinito que reciba una palabra por teclado y verifique si es palíndrome, 
 # termina la ejecución si se ingresa la palabra salir
+while True:
+    word = input ("Introduzca palabra a verificar: ")
+    word = word.lower()
+    if word == "salir":
+        break
+    else:
+        word_without_space = (word.replace(" ",""))
+        print ((word_without_space) == (word_without_space[::-1]))
