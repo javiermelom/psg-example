67c91f15260119c1adf9ed2d8d754142aeeaa361
javiermelom <javiermelom@hotmail.com>
2024-06-03 23:35:54 -0500

    Ejercicio 6 ciclo infinito numero primo

--- retos_sesion_13/ejercicio_06.py
+++ retos_sesion_13/ejercicio_06.py
@@ -1,2 +1,25 @@
 # Crea un ciclo infinito que reciba un número por teclado y verifique si es un número primo, 
 # termina la ejecución si se ingresa el número 0
+num = int (input ("Digite un numero: "))
+aux = 1
+cont = 0
+while True:
+    if num == 0:
+        break
+    else:
+        if num % aux == 0:
+            cont += 1
+            aux += 1
+            if aux >= num:
+                if cont == 2:
+                    print (f"el numero {num} si es primo")
+                    aux = 1
+                    cont = 0
+                    num = int (input ("Digite un numero: "))
+                else:
+                    print (f"El numero {num} no es primo")
+                    aux = 1
+                    cont = 0
+                    num = int (input ("Digite un numero: "))
+        else:
+            aux += 1    
