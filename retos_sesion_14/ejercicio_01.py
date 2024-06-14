1553574f2226f31369e4fc7d0899b6334a9ce35f
javiermelom <javiermelom@hotmail.com>
2024-06-14 15:14:49 -0500

    Ejercicio 01 Funciones, recorrer una lista con for

--- retos_sesion_14/ejercicio_01.py
+++ retos_sesion_14/ejercicio_01.py
@@ -1,2 +1,12 @@
 # Un estudiante desea saber cuál es su promedio de calificaciones en la materia de matemáticas, 
 # cree una función que reciba las calificaciones como lista y devuelva el promedio las calificaciones son 20,40,60,51,13
+
+def promedio_matematicas(calificaciones):
+    promedio = 0
+    for nota in calificaciones:
+        promedio += nota
+    return promedio / len(calificaciones)
+    
+lista = [20,40,60,51,13]
+result = promedio_matematicas(lista)
+print (result)
