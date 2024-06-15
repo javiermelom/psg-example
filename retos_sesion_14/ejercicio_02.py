3c583a9625858504afd382acde9851ce329c5ddb
javiermelom <javiermelom@hotmail.com>
2024-06-14 20:52:27 -0500

    Ejercicio 02 funciones con *args que sirve cuando no se sabe la cantidad de argumentos

--- retos_sesion_14/ejercicio_02.py
+++ retos_sesion_14/ejercicio_02.py
@@ -1,2 +1,19 @@
 # Calculadora flexible: Crea una calculadora que acepte diferentes operaciones matemáticas como argumentos de palabras 
 # clave y realice los cálculos correspondientes, las operaciones son suma, resta, multiplicación y división
+def calculadora (num1, num2, *operaciones):
+    resultado = ""
+    for operacion in operaciones:
+        if operacion == "suma":
+            resultado += (f"el resultado de la {operacion} es: {num1 + num2}\n")
+        elif operacion == "resta":
+            resultado += (f"el resultado de la {operacion} es: {num1 - num2}\n")
+        elif operacion == "multiplicacion":
+            resultado += (f"el resultado de la {operacion} es: {num1 * num2}\n")
+        elif operacion == "division":
+            resultado += (f"el resultado de la {operacion} es: {num1 / num2}\n")
+        else:
+            return ("operador invalido")
+    return resultado
+
+resultado = calculadora (5, 5, "multiplicacion", "suma", "resta", "division")
+print (resultado)
