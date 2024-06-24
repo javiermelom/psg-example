0e5c51eea4bd5acff63d257929372c7b5ce49a81
javiermelom <javiermelom@hotmail.com>
2024-06-24 06:08:21 -0500

    Ejercicio 04 funciones, funcion lambda o funcion anonima

--- retos_sesion_14/ejercicio_04.py
+++ retos_sesion_14/ejercicio_04.py
@@ -1,1 +1,6 @@
 # Crear una función anónima para obtener el área de un círculo con radio 5
+pi = 3.1416
+area_circulo = lambda radio_circulo: pi * radio_circulo**2
+
+radio_circ = 5
+print(f"El área del círculo con radio {radio_circ} es {area_circulo(radio_circ)}")
