ab612dcc868aee7a75e1bfd828234b6cff37a4c3
javiermelom <javiermelom@hotmail.com>
2024-06-24 06:14:29 -0500

    Ejercicio 05 funciones, funcion cadena invertida

--- retos_sesion_14/ejercicio_05.py
+++ retos_sesion_14/ejercicio_05.py
@@ -1,1 +1,7 @@
 # Crear una función que reciba una cadena y devuelva la cadena invertida
+def cadena_invertida (cadena):
+    return cadena[::-1]
+
+cadena = input("Ingrese cadena a invertir: ")
+resultado = cadena_invertida(cadena)
+print (f"La cadena invertida es: {resultado}")
