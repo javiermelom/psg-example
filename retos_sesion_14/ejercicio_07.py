e4f462345e9427657f72f066dae860b4589ea2a2
javiermelom <javiermelom@hotmail.com>
2024-06-24 06:35:36 -0500

    Ejercicio 07 funciones,triqui o 3 en raya no logre desarrollarlo

--- retos_sesion_14/ejercicio_07.py
+++ retos_sesion_14/ejercicio_07.py
@@ -1,2 +1,2 @@
 # Simular un tres en raya con funciones donde reciba las jugadas y devuelva el ganador hasta que alguien ingrese salir
-# Este ejercicio no lo pude realizar, intente pero se me complico mucho la logica
+print("Este ejercicio no lo pude realizar, intente pero se me complico mucho la logica")
