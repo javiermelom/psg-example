7ef67a9409f50cb45337b9180e164de174893085
javiermelom <javiermelom@hotmail.com>
2024-06-24 06:26:55 -0500

    Ejercicio 06 funciones, lista numeros pares

--- retos_sesion_14/ejercicio_06.py
+++ retos_sesion_14/ejercicio_06.py
@@ -1,1 +1,9 @@
 # Crear una función que reciba una lista de números y devuelva solo los números pares
+def num_pares(list_numeros):
+    return [num for num in list_numeros if num % 2 == 0]
+
+# list_numeros = list(input("Ingrese lista numeros: "))
+# print (list_numeros, type(list_numeros))
+list_numeros = [2,4,3,5,6,7,8,9,18,20,11,13,16]
+resultado = num_pares(list_numeros)
+print(f"Los números pares en la lista ingresada son {resultado}")
