2716c84f6da2cee969a4506417a7b3af680a096f
javiermelom <javiermelom@hotmail.com>
2024-06-24 06:02:12 -0500

    Ejercicio 03 funciones, recursividad serie fibonacci

--- retos_sesion_14/ejercicio_03.py
+++ retos_sesion_14/ejercicio_03.py
@@ -1,1 +1,10 @@
 # Crear una función recursiva para obtener el N número de la serie de Fibonacci
+
+def num_fibonacci(num):
+    if num <= 1:
+        return num
+    else:
+        return num_fibonacci(num-1) + num_fibonacci(num-2)
+
+num = int(input("Digite numero para calcular serie Fibonacci: "))
+print(f"El número {num} de la serie Fibonacci es {num_fibonacci(num)}")
